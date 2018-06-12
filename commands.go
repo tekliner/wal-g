31ffdc54b5722629d37b0db4c304a4dd89038abf
Andrey Inishev <inish777@gmail.com>
2018-06-12 20:38:05 +0700

    First commit to support symlinks for tablespaces

--- commands.go
+++ commands.go
@@ -18,7 +18,86 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/pkg/errors"
 	"sync"
+	"sort"
 )
+
+// walk recursively descends path, calling walkFn.
+func walk(path string, info os.FileInfo, walkFn filepath.WalkFunc) error {
+	if !info.IsDir() {
+		return walkFn(path, info, nil)
+	}
+
+	names, err := readDirNames(path)
+	err1 := walkFn(path, info, err)
+	// If err != nil, walk can't walk into this directory.
+	// err1 != nil means walkFn want walk to skip this directory or stop walking.
+	// Therefore, if one of err and err1 isn't nil, walk will return.
+	if err != nil || err1 != nil {
+		// The caller's behavior is controlled by the return value, which is decided
+		// by walkFn. walkFn may ignore err and return nil.
+		// If walkFn returns SkipDir, it will be handled by the caller.
+		// So walk should return whatever walkFn returns.
+		return err1
+	}
+
+	for _, name := range names {
+		filename := filepath.Join(path, name)
+		fileInfo, err := os.Lstat(filename)
+		if err != nil {
+			if err := walkFn(filename, fileInfo, err); err != nil && err != filepath.SkipDir {
+				return err
+			}
+		} else {
+			err = walk(filename, fileInfo, walkFn)
+			if err != nil {
+				if !fileInfo.IsDir() || err != filepath.SkipDir {
+					return err
+				}
+			}
+		}
+	}
+	return nil
+}
+
+// Walk walks the file tree rooted at root, calling walkFn for each file or
+// directory in the tree, including root. All errors that arise visiting files
+// and directories are filtered by walkFn. The files are walked in lexical
+// order, which makes the output deterministic but means that for very
+// large directories Walk can be inefficient.
+// Walk does not follow symbolic links.
+func Walk(root string, walkFn filepath.WalkFunc) error {
+	info, err := os.Lstat(root)
+	if info.Mode() & os.ModeSymlink != 0 {
+		symlinkPath, _ := os.Readlink(info.Name())
+		info, err = os.Lstat(symlinkPath)
+	}
+	if err != nil {
+		err = walkFn(root, nil, err)
+	} else {
+		err = walk(root, info, walkFn)
+	}
+	if err == filepath.SkipDir {
+		return nil
+	}
+	return err
+}
+
+// readDirNames reads the directory named by dirname and returns
+// a sorted list of directory entries.
+func readDirNames(dirname string) ([]string, error) {
+	f, err := os.Open(dirname)
+	if err != nil {
+		return nil, err
+	}
+	names, err := f.Readdirnames(-1)
+	f.Close()
+	if err != nil {
+		return nil, err
+	}
+	sort.Strings(names)
+	return names, nil
+}
+
 
 // HandleDelete is invoked to perform wal-g delete
 func HandleDelete(pre *Prefix, args []string) {
@@ -393,7 +472,7 @@ func HandleBackupPush(dirArc string, tu *TarUploader, pre *Prefix) {
 
 	bundle.StartQueue()
 	fmt.Println("Walking ...")
-	err = filepath.Walk(dirArc, bundle.TarWalker)
+	err = Walk(dirArc, bundle.TarWalker)
 	if err != nil {
 		log.Fatalf("%+v\n", err)
 	}
